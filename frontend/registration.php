0e1ef6f2b4774775502dee3fab19054207d0b96d
angge <166150697+anggevictoria@users.noreply.github.com>
2024-06-27 21:52:01 +0800

    Update registration.php

--- frontend/registration.php
+++ frontend/registration.php
@@ -1,52 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
-		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
-	</head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
 <body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="https://sourcecodester.com">Sourcecodester</a>
-		</div>
-	</nav>
-	<div class="col-md-3"></div>
-	<div class="col-md-6 well">
-		<h3 class="text-primary">PHP - PDO Login and Registration</h3>
-		<hr style="border-top:1px dotted #ccc;"/>
-		<div class="col-md-2"></div>
-		<div class="col-md-8">
-			<form action="../API/register_query.php" method="POST">	
-				<h4 class="text-success">Register here...</h4>
-				<hr style="border-top:1px groovy #000;">
-				
-				<div class="form-group">
-					<label>Firstname</label>
-					<input type="text" class="form-control" name="firstname" />
-				</div>
-				
-				<div class="form-group">
-					<label>Lastname</label>
-					<input type="text" class="form-control" name="lastname" />
-				</div>
-				
-				<div class="form-group">
-					<label>Username</label>
-					<input type="text" class="form-control" name="username" />
-				</div>
-				
-				<div class="form-group">
-					<label>Password</label>
-					<input type="password" class="form-control" name="password" />
-				</div>
-				<br />
-				<div class="form-group">
-					<button class="btn btn-primary form-control" name="register">Register</button>
-				</div>
-				
-				<a href="../index.php">Login</a>
-			</form>
-		</div>
-	</div>
+    <div class="login-box">
+        <form class= "form" action="../API/register_query.php" method="POST">
+          
+          <div class="user-box">
+            <input type="text" name="firstname" required="">
+            <label>First Name</label>
+          </div>
+
+          <div class="user-box">
+            <input type="text" name="lastname" required="">
+            <label>Last Name</label>
+          </div>
+
+          <div class="user-box">
+            <input type="text" name="username" required="">
+            <label>Username</label>
+          </div>
+          
+          <div class="user-box">
+            <input type="password" name="password" required="">
+            <label>Password</label>
+          </div>
+
+          <input type="submit" value="Register" name="register">
+        </form>
+        
+        <p class="text--center">Already have an account? <a href="../index.php">Proceed to Login</a> <svg class="icon">
 </body>
 </html>
+
